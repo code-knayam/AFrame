30e1597204b9a7c1c8e292bd2191919347bdf840
code-knayam <mayankjain1394@gmail.com>
2020-07-03 21:25:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static("public"));
+app.use("/scripts", express.static(`${__dirname}/node_modules/`));
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => {
+  console.log("listening on port %d", port);
+});
